400a74b9a8845a7f366d1fb77b6b593b10eaf360
sksachin7z2 <sksachin7z2@gmail.com>
2022-02-15 11:04:15 +0530

    fix bugs

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -13,31 +13,31 @@ function Contact() {
         <h2 className='text-center my-4'>Contact</h2>      
         <hr style={{height:"4px"}}/>
 <div className='App my-5'>
-      <div className="card mb-3" style={{backgroundColor:" rgb(0, 31, 40)",maxWidth:"576px",margin:"auto"}}>
+      <div className="card mb-3" style={{backgroundColor:"transparent",maxWidth:"576px",margin:"auto"}}>
       <div className="row g-0">
         
         <div className="col-md-12 query">
-          <div className="card-body">
-            <h5 className="card-title text-center darkmode">Contact Me</h5>
+          <div style={{border:"3px solid black",borderRadius:"5px"}} className="card-body">
+            <h5 className="card-title text-center ">Contact Me</h5>
            
             <div  className='App'>
             <form action="https://formsubmit.co/sksachin7z2@gmail.com" method="POST">
   <div className="form-group">
-    <label className='darkmode my-2' htmlFor="name">Name</label>
+    <label className=' my-2 font-weight' htmlFor="name">Name</label>
     <input type="text" className="form-control "  style={{backgroundColor:"white"}} id="name" name="name"  onChange={onchange} aria-describedby="emailHelp"   required/>
    
   </div>
   <div className="form-group">
-    <label className='darkmode my-2' htmlFor="email">Email address</label>
+    <label className=' my-2 font-weight' htmlFor="email">Email address</label>
     <input type="email" className="form-control "  style={{backgroundColor:"white"}} id="email" name="email" onChange={onchange} aria-describedby="emailHelp" required/>
    
   </div>
   <div className="form-group">
-  <label className='darkmode my-2' htmlFor="subject">Subject</label>
+  <label className=' my-2 font-weight' htmlFor="subject">Subject</label>
     <input type="text" className="form-control "  style={{backgroundColor:"white"}} id="subject" onChange={onchange} name="subject"   required/>
   </div>
   <div className="form-group">
-  <label className='darkmode my-2' htmlFor="description">Description</label><br />
+  <label className=' my-2 font-weight' htmlFor="description">Description</label><br />
     <textarea name="description" style={{backgroundColor:"white"}} id="description" className='form-control' cols="50" rows="10"></textarea>
   </div>
   
