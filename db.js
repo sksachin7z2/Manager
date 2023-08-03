ab8c6a05ccfe19a17f78dec02bc65589f8541c91
supratik <supratik20_ug@ece.nits.ac.in>
2023-08-03 21:46:58 +0530

    updated

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const mongoose=require("mongoose");
 require('dotenv').config()
 
-const mongoURI=`mongodb+srv://sksachin7z2:ramdulari123@cluster0.f01hh.mongodb.net/manager?authSource=admin&replicaSet=atlas-ppw553-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true`
+const mongoURI=`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.f01hh.mongodb.net/manager?authSource=admin&replicaSet=atlas-ppw553-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true`
 
 const connectToMongo=()=>{
     mongoose.connect(mongoURI,()=>{
